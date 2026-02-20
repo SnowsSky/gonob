2c3e86218bd004a028751cb58dd7f00ad828f974
SnowsSky <snowsky473@gmail.com>
2026-02-20 19:00:52 +0100

    .

--- aur/search.go
+++ aur/search.go
@@ -12,37 +12,38 @@ type AURResponse struct {
 }
 
 type AURResult struct {
-	Name       string  `json:"Name"`
-	Version    string  `json:"Version"`
-	Maintainer *string `json:"Maintainer"`
-	Popularity float64 `json:"Popularity"`
+	Name        string  `json:"Name"`
+	Version     string  `json:"Version"`
+	Maintainer  string  `json:"Maintainer"`
+	Description string  `json:"Description"`
+	Popularity  float64 `json:"Popularity"`
 }
 
-func Search(pkg string, to_install bool) (string, string, *string, float64, error) {
+func Search(pkg string, to_install bool) (string, string, string, float64, error) {
 	if to_install {
 		URL := "https://aur.archlinux.org/rpc.php?v=5&type=info&arg=" + pkg
 		response, err := http.Get(URL)
 		if err != nil {
-			return "", "", nil, 0, fmt.Errorf("==> ERROR: Unable to reach the AUR.")
+			return "", "", "", 0, fmt.Errorf("==> ERROR: Unable to reach the AUR.")
 		}
 		defer response.Body.Close()
 
 		if response.StatusCode != http.StatusOK {
-			return "", "", nil, 0, fmt.Errorf("==> ERROR: Unable to reach the AUR.")
+			return "", "", "", 0, fmt.Errorf("==> ERROR: Unable to reach the AUR.")
 		}
 
 		var aurResp AURResponse
 		if err := json.NewDecoder(response.Body).Decode(&aurResp); err != nil {
-			return "", "", nil, 0, fmt.Errorf("==> ERROR: %v", err)
+			return "", "", "", 0, fmt.Errorf("==> ERROR: %v", err)
 		}
 
 		if aurResp.ResultCount == 0 || len(aurResp.Results) == 0 {
-			return "", "", nil, 0, fmt.Errorf("==> ERROR: cannot find package %s in AUR", pkg)
+			return "", "", "", 0, fmt.Errorf("==> ERROR: cannot find package %s in AUR", pkg)
 		}
 
 		result := aurResp.Results[0]
 		return result.Name, result.Version, result.Maintainer, result.Popularity, nil
 
 	}
-	return "", "", nil, 0, nil
+	return "", "", "", 0, nil
 }
