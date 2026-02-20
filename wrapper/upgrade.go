c1e87c77abf227fa08a0066c5a4ade0e9422ea3e
SnowsSky <snowsky473@gmail.com>
2026-02-20 17:27:46 +0100

    .

--- wrapper/upgrade.go
+++ wrapper/upgrade.go
@@ -1,5 +1,37 @@
 package wrapper
 
-func Upgrade() {
+import (
+	"fmt"
 
+	"github.com/Morganamilo/go-pacmanconf"
+)
+
+func Upgrade() {
+	h, err := alpm.Initialize("/", "/var/lib/pacman")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer func() {
+		if err := h.Release(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}()
+
+	conf, _, err := pacmanconf.ParseFile("/etc/pacman.conf")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, repo := range conf.Repos {
+		fmt.Println(repo)
+		db, err := h.RegisterSyncDB(repo.Name, 0)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		db.SetServers(repo.Servers)
+	}
 }
