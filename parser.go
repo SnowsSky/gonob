1cbd5d6d194b939a5c7d0c3face6abbe1777ebb2
SnowsSky <snowsky473@gmail.com>
2026-02-20 22:29:52 +0100

    Fix: error when using 'gonob'

--- parser.go
+++ parser.go
@@ -8,7 +8,7 @@ import (
 
 func parser(args []string) {
 	if len(args) == 0 {
-		fmt.Println(aur.Yellow + "==> " + translations.Translate("warning_string") + " : " + translations.Translate("unknown_command"))
+		fmt.Println(aur.Yellow + "==> " + translations.Translate("warning_string") + " : " + translations.Translate("unknown_command") + aur.Reset)
 		return
 	}
 	switch args[0] {
@@ -17,6 +17,6 @@ func parser(args []string) {
 			aur.Install(args[2:])
 		}
 	default:
-		fmt.Println(aur.Yellow + "==> " + translations.Translate("warning_string") + " : " + translations.Translate("unknown_command"))
+		fmt.Println(aur.Yellow + "==> " + translations.Translate("warning_string") + " : " + translations.Translate("unknown_command") + aur.Reset)
 	}
 }
