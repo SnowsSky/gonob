6b5ae177af3c010596135449c4c69eaeb51eb5c0
SnowsSky <snowsky473@gmail.com>
2026-02-20 18:34:26 +0100

    Added checks before cloning

--- aur/install.go
+++ aur/install.go
@@ -8,6 +8,14 @@ import (
 
 var builddest string
 
+func CheckPkgFolder() bool {
+	if _, err := os.Stat(builddest); os.IsNotExist(err) {
+		// the folder does not exist.
+		return false
+	}
+	return true
+}
+
 func Install(pkgs []string) {
 	for _, pkg := range pkgs {
 		pkg_name, pkg_version, pkg_maintainer, pkg_popularity, err := Search(pkg, true)
@@ -17,19 +25,24 @@ func Install(pkgs []string) {
 		}
 		builddest = "/tmp/" + pkg_name
 		fmt.Println(pkg_name, pkg_version, pkg_maintainer, pkg_popularity)
-		fmt.Println("==> Cloning", pkg_name, "'s repository...")
-		cmd := exec.Command("git", "clone", fmt.Sprintf("https://aur.archlinux.org/%s.git", pkg_name), "/tmp/"+pkg_name)
 
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+		if !CheckPkgFolder() {
+			fmt.Println("==> Cloning", pkg_name, "'s repository...")
+			cmd := exec.Command("git", "clone", fmt.Sprintf("https://aur.archlinux.org/%s.git", pkg_name), "/tmp/"+pkg_name)
 
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println("==> ERROR: Failed to clone", pkg_name)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println("==> ERROR: Failed to clone", pkg_name)
+			}
+		} else {
+			fmt.Println("==> Warning:", pkg_name+"'s folder already exists, skipping...")
 		}
 
 		fmt.Println("==> Building", pkg_name+"...")
-		cmd = exec.Command("makepkg", "-si", "--noconfirm")
+		cmd := exec.Command("makepkg", "-si", "--noconfirm")
 		cmd.Dir = builddest
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
