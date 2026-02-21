56b892e3570949ff56f457c54be4ad390417899a
SnowsSky <snowsky473@gmail.com>
2026-02-21 23:53:57 +0100

    .

--- aur/update.go
+++ aur/update.go
@@ -2,7 +2,6 @@ package aur
 
 import (
 	"fmt"
-	"gonob/translations"
 	"log"
 
 	alpm "github.com/Jguer/dyalpm"
@@ -10,42 +9,33 @@ import (
 
 func Update() {
 	AurPackages := []string{}
-	Packages := make(map[string]struct{})
+
+	// Initialisation ALPM
 	handle, err := alpm.Initialize("/", "/var/lib/pacman")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Release()
+
+	// Base locale
 	localDB, err := handle.LocalDB()
 	if err != nil {
-		return
-	}
-	syncDBs, err := handle.SyncDBs()
-	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	for _, db := range syncDBs {
-		_ = db.PkgCache().ForEach(func(pkg alpm.Package) error {
-			Packages[pkg.Name()] = struct{}{}
-			return nil
-		})
-	}
-	err = localDB.PkgCache().ForEach(func(pkg alpm.Package) error {
-		if _, exists := Packages[pkg.Name()]; !exists {
-			AurPackages = append(AurPackages, pkg.Name(), pkg.Version())
-			fmt.Println("AUR:", pkg.Name())
-		}
-		return nil
-	})
+	// DB distantes
+	syncDBs, err := handle.SyncDBs()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	if len(AurPackages) == 0 {
-		fmt.Println(Green + "==> " + Reset + White + translations.Translate("no_aur_updates") + Reset)
+
+	for _, pkg := range localDB.PkgCache().Collect() {
+		for _, db := range syncDBs {
+			if db.Pkg(pkg.Name()) == nil {
+				AurPackages = append(AurPackages, pkg.Name())
+			}
+		}
 	}
-	for AurPackage := range AurPackages {
-		AurPackage = AurPackage
-	}
+	fmt.Println(AurPackages)
 
 }
